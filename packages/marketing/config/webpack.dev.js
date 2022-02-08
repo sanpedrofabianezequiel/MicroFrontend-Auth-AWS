ee5592a36faa312ec429b9eb2ee33af32437059c
sanpedro ezequiel <sanpedrofabianezequiel@gmail.com>
2022-02-07 23:15:50 -0300

    Update files

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -17,7 +17,7 @@ const devConfig = {
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap',
+        './MarketingRemote': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap',
+        './MarketingRemote': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
